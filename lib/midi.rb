3f58e983ddf82f03f527417e38525214d6e08738
Siim Põder <siim@p6drad-teel.net>
2020-06-27 18:33:03 +0100

    midi_record: also record velocities and durations

--- lib/midi.rb
+++ lib/midi.rb
@@ -28,25 +28,45 @@ define :play_midi do |&block|
   end
 end
 
-define :record_midi do |duration, quantum: 0.25|
+define :record_midi do |duration, time_quantum: 0.25, velocity_quantum: 0.2|
   start = vt
   times = []
   notes = []
+  velocities = []
+  durations = []
+  playing = {}
+
   name = "_record_midi_#{vt}".to_sym
   live_loop name do
     n, v = sync "/midi:#{get :_midi_input}/note_on"
-    next if v == 0
     t = vt - start - current_sched_ahead_time
-    t = (t / quantum).round * quantum
+    t = (t / time_quantum).round * time_quantum
+    d = nil
+
+    if v == 0 then
+      if not playing[n].nil? then
+        playing[n][0] = t - playing[n][0]
+        playing.delete(n)
+      end
+      next
+    else
+      d = [t]
+      playing[n] = d
+    end
+
     if t >= duration then
       if times.length > 0 then
         puts "times = " + times.to_s
         puts "notes = " + notes.to_s
+        puts "velocities = " + velocities.to_s
+        puts "durations = " + durations.map { |d| d[0] }.to_s
       end
       stop
     else
       times << t
       notes << note_info(n).midi_string.downcase.to_sym
+      velocities << (((v.to_f / 127) / velocity_quantum).round * velocity_quantum).round(4)
+      durations << d
     end
   end
 end
