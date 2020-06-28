7abab1c8739274e7080f517833385cafccbc720b
Siim Põder <siim@p6drad-teel.net>
2020-06-28 14:54:11 +0100

    keyboard: handle of arrays, rings, note-alikes

--- lib/keyboard.rb
+++ lib/keyboard.rb
@@ -1,17 +1,27 @@
-def keyboard_keys(notes, from: nil)
+define :keyboard_keys do |notes, from: nil|
   underline = "\u0332"
   white = "\u25a1"
   black = "\u25a0"
 
-  notes = [notes] if not notes.is_a? Array
+  case
+  when (notes.is_a? Array)
+  when (notes.is_a? SonicPi::Core::RingVector)
+    notes = notes.to_a
+  when (notes.is_a? Numeric)
+    notes = [notes]
+  when (notes.is_a? Symbol)
+    notes = [notes]
+  else
+    return notes.class.to_s
+  end
+  notes = notes.map { |n| note(n) }
 
-  numeric = notes.map { |n| note(n) }
-  minimum = numeric.min
+  minimum = notes.min
   if not from.nil? and note(from) < minimum then
     minimum = note(from)
   end
   first = (minimum / 12) * 12
-  last = (numeric.max / 12 + 1) * 12 - 1
+  last = (notes.max / 12 + 1) * 12 - 1
   
   info = note_info(first)
   buf = info.midi_string + ": "
@@ -19,20 +29,19 @@ def keyboard_keys(notes, from: nil)
     if n % 12 == 0 && n != first then
       buf += "|"
     end
-    if numeric.include?(n) then
+    if notes.include?(n) then
       buf += underline
     end
-    info = note_info(n)
-    if info.midi_string.length == 2 then
-      buf += white
-    else
+    if [1,3,6,8,10].include?(n % 12) then
       buf += black
+    else
+      buf += white
     end
   end
 
   return buf
 end
 
-def keyboard(*args, **kwargs)
+define :keyboard do |*args, **kwargs|
   puts keyboard_keys(*args, **kwargs)
 end
